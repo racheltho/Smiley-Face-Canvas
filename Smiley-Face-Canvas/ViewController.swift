579c7a8ab2df5ffda5300fd47367edf84eb63f72
Rachel Thomas <racheltho@gmail.com>
2015-03-03 18:37:54 -0800

    Initial Commit

---
+++ Smiley-Face-Canvas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smiley-Face-Canvas
+//
+//  Created by Rachel Thomas on 3/3/15.
+//  Copyright (c) 2015 Rachel Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
