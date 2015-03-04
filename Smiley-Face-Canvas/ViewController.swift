a9380ecb0a458b683b197073fb2385f12f0c2e47
Rachel Thomas <racheltho@gmail.com>
2015-03-03 21:00:55 -0800

    panGestureRecognizer for tray working

--- Smiley-Face-Canvas/ViewController.swift
+++ Smiley-Face-Canvas/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var trayOriginalCenter: CGPoint!
+    var start_point: CGPoint!
+    
+    @IBOutlet var screenView: UIView!
+    @IBOutlet weak var trayView: UIView!
+    
+    @IBAction func onTrayPanGesture(panGestureRecognizer: UIPanGestureRecognizer) {
+        
+        var point = panGestureRecognizer.locationInView(screenView)
+        
+        switch panGestureRecognizer.state {
+        case UIGestureRecognizerState.Began:
+            start_point = point
+            trayOriginalCenter = trayView.center
+            println("trayOriginalCenter: \(trayOriginalCenter.y)")
+            println("Gesture began at: \(start_point.y)")
+        case UIGestureRecognizerState.Changed:
+            println("Gesture changed at: \(point.y)")
+            var ytranslation = point.y - start_point.y
+            println("translation: \(ytranslation)")
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + ytranslation)
+        case UIGestureRecognizerState.Ended:
+            println("Gesture ended at: \(point)")
+        default:
+            break
+        }
+    
+    }
+
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+
     }
 
     override func didReceiveMemoryWarning() {
