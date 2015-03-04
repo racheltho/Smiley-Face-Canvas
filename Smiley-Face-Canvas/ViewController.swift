f41d84ff27dd20249a1b3edf89d28492ba7d0478
Rachel Thomas <racheltho@gmail.com>
2015-03-04 10:20:35 -0800

    tray working correctly, starting to add smiley faces

--- Smiley-Face-Canvas/ViewController.swift
+++ Smiley-Face-Canvas/ViewController.swift
@@ -11,32 +11,42 @@ import UIKit
 class ViewController: UIViewController {
 
     var trayOriginalCenter: CGPoint!
-    var start_point: CGPoint!
+    var trayUpCenter: CGPoint!
+    var trayDownCenter: CGPoint!
+    var startPoint: CGPoint!
+    var newlyCreatedFace: UIImageView!
     
     @IBOutlet var screenView: UIView!
     @IBOutlet weak var trayView: UIView!
     
+    @IBAction func dragFace(sender: UIPanGestureRecognizer) {
+    
+    
+    }
+    
     @IBAction func onTrayPanGesture(panGestureRecognizer: UIPanGestureRecognizer) {
         
         var point = panGestureRecognizer.locationInView(screenView)
+        var velocity = panGestureRecognizer.velocityInView(screenView)
         
         switch panGestureRecognizer.state {
         case UIGestureRecognizerState.Began:
-            start_point = point
+            startPoint = point
             trayOriginalCenter = trayView.center
-            println("trayOriginalCenter: \(trayOriginalCenter.y)")
-            println("Gesture began at: \(start_point.y)")
         case UIGestureRecognizerState.Changed:
-            println("Gesture changed at: \(point.y)")
-            var ytranslation = point.y - start_point.y
-            println("translation: \(ytranslation)")
+            var ytranslation = point.y - startPoint.y
             trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + ytranslation)
         case UIGestureRecognizerState.Ended:
-            println("Gesture ended at: \(point)")
+            if velocity.y > 0 {
+                trayView.center = trayDownCenter            }
+            if velocity.y < 0 {
+                trayView.center = trayUpCenter
+            }
         default:
             break
         }
-    
+        screenView.layoutIfNeeded()
+        trayView.layoutIfNeeded()
     }
 
 
@@ -44,8 +54,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
+        trayUpCenter = trayView.center
+        trayDownCenter = trayView.center
+        let trayHeight = trayView.frame.height
+        trayDownCenter.y = trayUpCenter.y + trayHeight - 36
     }
 
     override func didReceiveMemoryWarning() {
